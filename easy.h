b307550d989c14b990613be8fb8d0da2bb1101c4
blackwhitebear <60433367+sid4202@users.noreply.github.com>
2023-09-11 09:53:11 +0300

    Update easy.h

--- easy.h
+++ easy.h
@@ -1,8 +1,23 @@
 #ifndef EASY_H_INCLUDED
 #define EASY_H_INCLUDED
 
+void itc_name();
+void itc_fio();
+int itc_abs(int num);
+double itc_fabs(double num);
+int itc_revnbr(int num);
+bool itc_iseven(int num);
+int itc_max(int num, int num2);
+int itc_min(int num1, int num2);
+double itc_fmax(double num, double num2);
+double itc_fmin(double num, double num2);
 int itc_sqrt(int num);
+int itc_skv(int num);
+int itc_spr(int a, int b);
 int itc_str(int a, int b, int c);
-int itc_pow(int num, int step);
+double itc_scir(int radius);
+double itc_pow(int num, int step);
+bool itc_ispositive(int num);
+bool itc_ispositive_d(double num);
 
 #endif // EASY_H_INCLUDED
